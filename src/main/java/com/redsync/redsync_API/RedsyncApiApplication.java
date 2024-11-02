9682d51f62debc359196260fc8f9fdf16823a771
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2024-11-01 23:02:59 -0300

    Primer commit: agregar archivos del proyecto y controller por defecto

---
+++ src/main/java/com/redsync/redsync_API/RedsyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.redsync.redsync_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedsyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedsyncApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/redsync/redsync_API/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.redsync.redsync_API.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Redsync API!";
+    }
+}
